c9d974cf77f1a4c26810dd85d6ae40b16ac97b46
Oula Osman Abou <ola.syria.ab@gmail.com>
2024-07-26 21:33:05 +0200

    fixed a bug: previously clicked column is deleted

--- static/index.js
+++ static/index.js
@@ -1,5 +1,6 @@
 const svg = d3.select('svg');
 let allData = [];
+let clickedData = []; // Track data from clicked rectangles
 
 // Function to create visualization
 function createVisualization(data) {
@@ -15,26 +16,7 @@ function createVisualization(data) {
         .attr('height', d => d.height)
         .attr('width', d => d.width)
         .attr('fill', d => d.fill)
-        .on('click', function( d) {
-            console.log(d.data);
-
-            fetch('/post_bar_data', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(d.data)
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Response from server:', data);
-                // Add new data to the visualization
-                appendNewData(data.result);
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-        });
+        .on('click', handleCsvRectClick); // Use CSV click handler
 
     // Append the enter selection to the DOM
     rects.enter()
@@ -44,38 +26,45 @@ function createVisualization(data) {
             .attr('height', d => d.height)
             .attr('width', d => d.width)
             .attr('fill', d => d.fill)
-            .on('click', function( d) {
-                console.log(d.data);
-
-                fetch('/post_bar_data', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(d.data)
-                })
-                .then(response => response.json())
-                .then(data => {
-                    console.log('Response from server:', data);
-                    // Add new data to the visualization
-                    appendNewData(data.result);
-                })
-                .catch(error => {
-                    console.error('Error:', error);
-                });
-            });
+            .on('click', handleCsvRectClick); // Use CSV click handler
 
     // Remove any rects that are no longer in the data
     rects.exit().remove();
 }
 
+// CSV rect click handler
+function handleCsvRectClick(d) {
+    console.log(d.data);
+
+    fetch('/post_bar_data', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(d.data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Response from server:', data);
+        // Add new data to the visualization
+        appendNewData(data.result);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
 // Function to append new data to the existing visualization
 function appendNewData(newData) {
-    allData = allData.concat(newData); // Combine new data with existing data
+    // Clear previous clickedData
+    clickedData = newData.map(d => ({...d, isClicked: true})); // Tag new data as clicked
 
-    // Join the new data to rects
+    // Update allData to remove previously clicked data
+    allData = allData.filter(d => !d.isClicked).concat(clickedData); // Combine only non-clicked data with new clicked data
+
+    // Join the combined data
     const rects = svg.selectAll('rect')
-        .data(allData);
+        .data(allData, d => d.id);
 
     // Update the attributes of existing rects
     rects.attr('y', d => d.y)
@@ -87,34 +76,59 @@ function appendNewData(newData) {
     // Append new rects
     rects.enter()
         .append('rect')
+            .attr('class', d => d.isClicked ? 'clicked' : '')
             .attr('y', d => d.y)
             .attr('x', d => d.x)
             .attr('height', d => d.height)
             .attr('width', d => d.width)
             .attr('fill', d => d.fill)
-            .on('click', function( d) {
-                console.log(d.data);
+            .on('click', handleClickedRectClick); // Use clicked rect click handler
 
-                fetch('/post_bar_data', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify(d.data)
-                })
-                .then(response => response.json())
-                .then(data => {
-                    console.log('Response from server:', data);
-                    // Add new data to the visualization
-                    appendNewData(data.result);
-                })
-                .catch(error => {
-                    console.error('Error:', error);
-                });
-            });
-
-    // Remove rects that are no longer in the data
+    // Remove previous clicked rects
     rects.exit().remove();
+
+    // Join the new clicked data for text elements
+    const texts = svg.selectAll('text')
+        .data(clickedData, d => d.id);
+
+    // Update existing text elements
+    texts.attr('x', d => d.x + d.width / 2)
+        .attr('y', d => d.y + d.height / 2)
+        .text(d => d.value);
+
+    // Append new text elements
+    texts.enter()
+        .append('text')
+            .attr('x', d => d.x + d.width / 2)
+            .attr('y', d => d.y + d.height / 2)
+            .attr('text-anchor', 'middle')
+            .attr('dy', '.35em')
+            .text(d => d.value);
+
+    // Remove old text elements
+    texts.exit().remove();
+}
+
+// Clicked rect click handler
+function handleClickedRectClick(d) {
+    console.log(d.data);
+
+    fetch('/post_bar_data', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(d.data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Response from server:', data);
+        // Add new data to the visualization
+        appendNewData(data.result);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 }
 
 // Create buttons and fetch data as before
